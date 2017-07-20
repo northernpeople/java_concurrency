37bb87d6ec0b2828df0bfd2f21ee3400667ffa07
Stepan Razdobarov <northernpeople@gmail.com>
2017-07-19 22:16:21 -0700

    added Runnable and executor example

---
+++ src/c/s/basics/CallableFutureDemo.java
@@ -0,0 +1,10 @@
+package c.s.basics;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class CallableFutureDemo {
+	
+
+
+}
---
+++ src/c/s/basics/RunnableExecutorDemo.java
@@ -0,0 +1,20 @@
+package c.s.basics;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class RunnableExecutorDemo {
+
+	/*
+	 * Executors are various convenience classes, often implementing a kind of thread pool.
+	 * We can submit Runnable tasks to be executed by the executor.
+	 */
+	public static void main(String[] args) {
+		Runnable task1 = () -> System.out.println("task1");
+		Runnable task2 = () -> System.out.println("task2");
+		
+		Executor executor = Executors.newCachedThreadPool();
+		executor.execute(task1);
+		executor.execute(task2);
+	}
+}
